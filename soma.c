d6ea8502665ae616bc32b7590ad4d36463153efe
Generoso2 <161092418+Generoso2@users.noreply.github.com>
2024-05-22 04:36:08 +0000

    Soma de arry criada

---
+++ soma.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int tam_array;
+    int i;
+    int soma = 0;
+
+    printf("Digite o tamanho do array: ");
+    scanf("%d", &tam_array);
+
+    int array[tam_array];
+
+    for (i = 0; i < tam_array; i++) {
+        printf("Digite o elemento %d: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+
+    for (i = 0; i < tam_array; ++i){
+        soma += array[i];
+    }
+
+    printf("A soma dos elementos do array é: %d", soma);
+
+    return 0;
+}
